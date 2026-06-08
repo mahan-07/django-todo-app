4f5d4e85d4813519f27cc0ef9d4533a46684d921
mahan <mahan.shahveisi.morad@gmail.com>
2026-06-08 18:19:38 +0330

    showing todos in the front and some css changes

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Todo
 # Create your views here.
 
 @login_required
 def home(request):
-    context = {}
+    user = request.user
+    todos = user.todos.all()
+    context = {"todos": todos}
     return render(request, 'index.html', context)
+
